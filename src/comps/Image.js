14b5f0ba39eed0db22440f8cbadc99bc84aa4ad5
freeman <freeman23@bigmir.net>
2020-03-02 13:43:10 +0200

    inittial

---
+++ src/comps/Image.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { GridList, GridTile } from "material-ui/GridList";
+import IconButton from "material-ui/IconButton";
+import ZoomIn from "material-ui/svg-icons/action/zoom-in";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+
+export default class Image extends Component {
+  state = {
+    open: false,
+    currentImg: ""
+  };
+  handleOpen = img => this.setState({ open: true, currentImg: img });
+  handleClose = () => this.setState({ open: false });
+  render() {
+    let imageListContent;
+    const { images } = this.props;
+    if (images) {
+      imageListContent = (
+        <GridList cols={3}>
+          {images.map(img => (
+            <GridTile
+              title={img.tags}
+              key={img.id}
+              subtitle={<span>by {img.user}</span>}
+              actionIcon={
+                <IconButton onClick={() => this.handleOpen(img.largeImageURL)}>
+                  <ZoomIn color="white"></ZoomIn>
+                </IconButton>
+              }
+            >
+              <img src={img.largeImageURL} alt="" />
+            </GridTile>
+          ))}
+        </GridList>
+      );
+    } else {
+      imageListContent = null;
+    }
+
+    const actions = [
+      <FlatButton label="Close" primary={true} onClick={this.handleClose} />
+    ];
+    return (
+      <div>
+        {imageListContent}
+        <Dialog
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          <img src={this.state.currentImg} style={{ width: "100%" }} alt="" />
+        </Dialog>
+      </div>
+    );
+  }
+}
+Image.propTypes = {
+  images: PropTypes.array.isRequired
+};
